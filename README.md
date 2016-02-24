b4553a97bf8ac7b750fd9789cbef0175c35bbab7
agulado <9948686@qq.com>
2016-02-24 18:07:17 +0800

    v1.2.1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# RotatingBanner JS插件 v1.1.1
+# RotatingBanner JS插件 v1.2.1
 ###轮播Banner（不含样式）
 
 文件结构：
@@ -16,19 +16,37 @@
 页面底部调用初始化方法并传参：
 
 		$(function() {
+			// Banner1
         	var RotatingBanner_obj = {
-        		autoPlay: null, // 自动播放：left/right/null，默认值：left
+		        autoPlay: null, // 自动播放：left/right/null，默认值：null
 		        box_selector: null, // 外盒选择器，默认值：section.banner
-		        pic_ul_selector: null, // 图片li的ul盒选择器，默认值：ul.pic_ul
-		        point_ul_selector: null, // 圆点li的ul盒选择器，空字符串为无圆点。默认值：ul.point_ul。
-		        point_li_selected_className:null, // 圆点高亮li的className，默认值：selected
+		        pic_ul_selector: null, // 图片li的ul盒选择器，此盒必须存在于box_selector中，且值中不用包含box_selector。默认值：ul.pic_ul
+		        point_ul_selector: null, // 圆点li的ul盒选择器，空字符串为无圆点。此盒不必存在于box_selector中。默认值：section.banner ul.point_ul。
+		        point_li_selected_className: null, // 圆点高亮li的className，默认值：selected
+		        arrow_left_selector: null, // 左箭头的盒选择器，此盒不必存在于box_selector中。null为无左箭头。默认值：null
+		        arrow_right_selector: null, // 右箭头的盒选择器，此盒不必存在于box_selector中。null为无右箭头。默认值：null
 		        duration: null, // 动画过渡时间，毫秒。默认500
 		        delay: null // 动画间隔，毫秒。默认5000
         	};
-	        RotatingBanner.init(RotatingBanner_obj);
+	        var RotatingBanner_1 = RotatingBanner.clone(RotatingBanner);
+	        RotatingBanner_1.init(RotatingBanner_obj);
+
+	        // Banner2
+	        RotatingBanner_obj = { };
+	        var RotatingBanner_2 = RotatingBanner.clone(RotatingBanner);
+	        RotatingBanner_2.init(RotatingBanner_obj);
 	    });
 
 更新历史：
 --------------
+v1.2.1：
+
+1. 修改参数 pic_ul_selector和point_ul_selector注释。
+2. 圆点li的选择器，不再固定于box_selector内，方便调用时灵活放置圆点（比如可以小缩略图的形式展现大图，点击则切换到相应大图）。
+3. 上一个版本中的demo在demo.html页传参测试忘复原了，已复原。
+4. 同一页面支持多个轮播。相应的，修改功能配置及启用代码。
+5. 增加左右箭头，点击时滚动一屏。
+6. 修改demo
+
 v1.1.1：
 增加参数：圆点高亮li的样式名
--- dist/jq/RotatingBanner.js
+++ dist/jq/RotatingBanner.js
@@ -7,42 +7,69 @@ var RotatingBanner = {
     box_width_px: null, // 外盒宽度，后续程序中获得
     // 参数集
     paras: {
-        autoPlay: null, // 自动播放：left/right/null，默认值：left
+        autoPlay: null, // 自动播放：left/right/null，默认值：null
         box_selector: null, // 外盒选择器，默认值：section.banner
-        pic_ul_selector: null, // 图片li的ul盒选择器，默认值：ul.pic_ul
-        point_ul_selector: null, // 圆点li的ul盒选择器，空字符串为无圆点。默认值：ul.point_ul。
+        pic_ul_selector: null, // 图片li的ul盒选择器，此盒必须存在于box_selector中，且值中不用包含box_selector。默认值：ul.pic_ul
+        point_ul_selector: null, // 圆点li的ul盒选择器，空字符串为无圆点。此盒不必存在于box_selector中。默认值：section.banner ul.point_ul。
         point_li_selected_className: null, // 圆点高亮li的className，默认值：selected
+        arrow_left_selector: null, // 左箭头的盒选择器，此盒不必存在于box_selector中。null为无左箭头。默认值：null
+        arrow_right_selector: null, // 右箭头的盒选择器，此盒不必存在于box_selector中。null为无右箭头。默认值：null
         duration: null, // 动画过渡时间，毫秒。默认500
         delay: null // 动画间隔，毫秒。默认5000
     },
-    init: function(paras) {
+    init: function(_paras) {
 
         var paras_default = {
-            autoPlay: "left",
+            autoPlay: null,
             box_selector: "section.banner",
             pic_ul_selector: "ul.pic_ul",
-            point_ul_selector: "ul.point_ul",
+            point_ul_selector: "section.banner ul.point_ul",
             point_li_selected_className: "selected",
+            arrow_left_selector: null,
+            arrow_right_selector: null,
             duration: 500,
             delay: 5000
         };
-        RotatingBanner.paras = paras = $.extend(paras_default, paras);
+        this.paras = $.extend(paras_default, _paras);
+        _paras = this.clone(this.paras);
 
-        RotatingBanner.banner_count = $(paras.box_selector + " " + paras.pic_ul_selector + " li").length;
+        this.banner_count = $(_paras.box_selector + " " + _paras.pic_ul_selector + " li").length;
 
-        RotatingBanner.resize();
+        this.resize(this);
 
         // 监听圆点
-        if (paras.point_ul_selector != "")
-            RotatingBanner.PointListener();
+        if (_paras.point_ul_selector != "")
+            this.PointListener(this);
 
-        if (paras.autoPlay)
-            RotatingBanner.preRotating(paras.autoPlay.toLowerCase());
+        // 监听左箭头
+        if (_paras.arrow_left_selector)
+            this.arrowLeftListener(this);
+
+        // 监听右箭头
+        if (_paras.arrow_right_selector)
+            this.arrowRightListener(this);
+
+        // 轮播
+        if (_paras.autoPlay)
+            this.preRotating(this, _paras.autoPlay.toLowerCase(this));
 
     },
 
+    // 克隆对象。用于一个页面中有多个轮播图时
+    clone: function(myObj) {
+        if (typeof(myObj) != 'object') return myObj;
+        if (myObj == null) return myObj;
+
+        var myNewObj = new Object();
+
+        for (var i in myObj)
+            myNewObj[i] = this.clone(myObj[i]);
+
+        return myNewObj;
+    },
+
     // 重定义盒尺寸
-    resize: function() {
+    resize: function(this_obj) {
         var n = 0; // 解决某些浏览器resize执行两遍的bug
 
         $(window).resize(function() {
@@ -53,12 +80,12 @@ var RotatingBanner = {
 
         var resize_do = function() {
 
-            setTimeout(function() {
+            var _paras = this_obj.clone(this_obj.paras);
+            var box_obj = $(_paras.box_selector); // 盒对象
+            var box_width_px = this_obj.box_width_px = box_obj.css("width").replace("px", "");
+            var box_height_px = box_obj.css("height").replace("px", "");
 
-                var paras = RotatingBanner.paras;
-                var box_obj = $(paras.box_selector); // 盒对象
-                var box_width_px = RotatingBanner.box_width_px = box_obj.css("width").replace("px", "");
-                var box_height_px = box_obj.css("height").replace("px", "");
+            setTimeout(function() {
 
                 var pic_li_obj = $(box_obj.find("ul.pic_ul li")); // 图片li对象
 
@@ -77,43 +104,47 @@ var RotatingBanner = {
 
         };
 
-        resize_do();
+        resize_do(this_obj);
     },
 
     // 轮播准备
     // direct：left/right
-    preRotating: function(direct) {
-        clearTimeout(RotatingBanner.Timeout_id);
+    preRotating: function(this_obj, direct) {
+        clearTimeout(this_obj.Timeout_id);
         switch (direct.toLowerCase()) {
             case "left":
             default:
-                RotatingBanner.Timeout_id = setTimeout(RotatingBanner.scrollToLeft, RotatingBanner.paras.delay);
+                this_obj.Timeout_id = setTimeout(function() {
+                    this_obj.scrollToLeft(this_obj);
+                }, this_obj.paras.delay);
                 break;
             case "right":
-                RotatingBanner.Timeout_id = setTimeout(RotatingBanner.scrollToRight, RotatingBanner.paras.delay);
+                this_obj.Timeout_id = setTimeout(function() {
+                    this_obj.scrollToRight(this_obj)
+                }, this_obj.paras.delay);
                 break;
         }
     },
 
     // 向左滚X屏
-    scrollToLeft: function(X) {
-        if (RotatingBanner.Rotating)
+    scrollToLeft: function(this_obj, X) {
+        if (this_obj.Rotating)
             return;
-        RotatingBanner.Rotating = true;
+        this_obj.Rotating = true;
 
         if (!X)
             X = 1;
 
-        var paras = RotatingBanner.paras;
-        var ul_obj = $(paras.box_selector + " " + paras.pic_ul_selector);
+        var _paras = this_obj.clone(this_obj.paras);
+        var ul_obj = $(_paras.box_selector + " " + _paras.pic_ul_selector);
 
         // 计算滚动后的left值
-        var ul_left_px_new = parseFloat(ul_obj.css("left").replace("px", "")) - X * RotatingBanner.box_width_px;
+        var ul_left_px_new = parseFloat(ul_obj.css("left").replace("px", "")) - X * this_obj.box_width_px;
 
         // 执行滚动
         ul_obj.animate({
             "left": ul_left_px_new
-        }, paras.duration, function() {
+        }, _paras.duration, function() {
             var li_obj = $(ul_obj.find("li"));
             var i = 0;
             for (; i < X; i++)
@@ -121,31 +152,31 @@ var RotatingBanner = {
             ul_obj.css("left", 0);
 
             // 切换banner_now
-            RotatingBanner.banner_now += X;
-            if (RotatingBanner.banner_now >= RotatingBanner.banner_count)
-                RotatingBanner.banner_now = 0;
+            this_obj.banner_now += X;
+            if (this_obj.banner_now >= this_obj.banner_count)
+                this_obj.banner_now = 0;
 
             // 切换圆点
-            RotatingBanner.changePoint();
+            this_obj.changePoint(this_obj);
 
-            RotatingBanner.Rotating = false;
+            this_obj.Rotating = false;
 
             // 再次执行滚动（如autoPlay不为null）
-            if (paras.autoPlay)
-                RotatingBanner.preRotating(paras.autoPlay.toLowerCase());
+            if (_paras.autoPlay)
+                this_obj.preRotating(this_obj, _paras.autoPlay.toLowerCase());
         });
     },
 
     // 向右滚X屏
-    scrollToRight: function(X) {
-        if (RotatingBanner.Rotating)
+    scrollToRight: function(this_obj, X) {
+        if (this_obj.Rotating)
             return;
-        RotatingBanner.Rotating = true;
+        this_obj.Rotating = true;
 
         if (!X)
             X = 1;
-        var paras = RotatingBanner.paras;
-        var ul_obj = $(paras.box_selector + " " + paras.pic_ul_selector);
+        var _paras = this_obj.clone(this_obj.paras);
+        var ul_obj = $(_paras.box_selector + " " + _paras.pic_ul_selector);
 
         var li_obj = $(ul_obj.find("li"));
         var li_obj_len = li_obj.length;
@@ -153,50 +184,69 @@ var RotatingBanner = {
         var i = 0;
         for (; i < X; i++)
             $(li_obj[li_obj_len - 1]).prependTo(ul_obj);
-        ul_obj.css("left", -X * RotatingBanner.box_width_px);
+        ul_obj.css("left", -X * this_obj.box_width_px);
 
         ul_obj.animate({
             "left": 0
-        }, paras.duration, function() {
+        }, _paras.duration, function() {
 
             // 切换banner_now
-            RotatingBanner.banner_now -= X;
-            if (RotatingBanner.banner_now < 0)
-                RotatingBanner.banner_now = RotatingBanner.banner_count - 1;
+            this_obj.banner_now -= X;
+            if (this_obj.banner_now < 0)
+                this_obj.banner_now = this_obj.banner_count - 1;
 
             // 切换圆点
-            RotatingBanner.changePoint();
+            this_obj.changePoint(this_obj);
 
-            RotatingBanner.Rotating = false;
+            this_obj.Rotating = false;
 
             // 再次执行滚动（如autoPlay不为null）
-            if (paras.autoPlay)
-                RotatingBanner.preRotating(paras.autoPlay.toLowerCase());
+            if (_paras.autoPlay)
+                this_obj.preRotating(this_obj, _paras.autoPlay.toLowerCase());
         });
     },
 
     // 切换圆点高亮
-    changePoint: function() {
-        var paras = RotatingBanner.paras;
-        var obj = $(paras.box_selector + " " + paras.point_ul_selector + " li");
-        obj.siblings("." + paras.point_li_selected_className).removeClass(paras.point_li_selected_className);
-        $(obj[RotatingBanner.banner_now]).addClass(paras.point_li_selected_className);
+    changePoint: function(this_obj) {
+        var _paras = this_obj.clone(this_obj.paras);
+        var obj = $(_paras.point_ul_selector + " li");
+        obj.siblings("." + _paras.point_li_selected_className).removeClass(_paras.point_li_selected_className);
+        $(obj[this_obj.banner_now]).addClass(_paras.point_li_selected_className);
     },
 
     // 监听圆点
-    PointListener: function() {
-        var paras = RotatingBanner.paras;
-        var obj = $(paras.box_selector + " " + paras.point_ul_selector + " li");
-        $(obj[0]).addClass(paras.point_li_selected_className);
-        obj.click(function() {
+    PointListener: function(this_obj) {
+        var _paras = this.clone(this_obj.paras);
+        var obj = $(_paras.point_ul_selector + " li");
+        $(obj[0]).addClass(_paras.point_li_selected_className);
+        obj.on("touchstart mousedown", function(event) {
+            event.preventDefault();
             var n = $(this).index();
-            if (n == RotatingBanner.banner_now)
+            if (n == this_obj.banner_now)
                 return;
             // clearTimeout();
-            if (n < RotatingBanner.banner_now)
-                RotatingBanner.scrollToRight(RotatingBanner.banner_now - n);
-            if (n > RotatingBanner.banner_now)
-                RotatingBanner.scrollToLeft(n - RotatingBanner.banner_now);
+            if (n < this_obj.banner_now)
+                this_obj.scrollToRight(this_obj, this_obj.banner_now - n);
+            if (n > this_obj.banner_now)
+                this_obj.scrollToLeft(this_obj, n - this_obj.banner_now);
+        });
+    },
+
+    // 监听左箭头
+    arrowLeftListener: function(this_obj) {
+        var _paras = this_obj.clone(this_obj.paras);
+        $(_paras.arrow_left_selector).on("touchstart mousedown", function(event) {
+            event.preventDefault();
+            this_obj.scrollToRight(this_obj);
+        });
+    },
+
+    // 监听右箭头
+    arrowRightListener: function(this_obj) {
+        var _paras = this_obj.clone(this_obj.paras);
+        $(_paras.arrow_right_selector).on("touchstart mousedown", function(event) {
+            event.preventDefault();
+            this_obj.scrollToLeft(this_obj);
         });
     }
 }
